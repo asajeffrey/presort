bddc0f0a11bb86ff3dc0c88f3a2b8223b14477ca
Alan Jeffrey <ajeffrey@mozilla.com>
2016-07-22 16:02:34 -0500

    First cut rustdoc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,45 @@
+//! A crate for presorted vectors.
+//!
+//! Presorted vectors provide similar functionality to vectors, but are designed
+//! to make sorting cheap in the case that mutating the vector preserves sort
+//! order.
+//!
+//! Define an update establishes sorting as:
+//!
+//! * `vec.presort()` establishes sorting.
+//!
+//! * `vec.sorted_iter()` establishes sorting.
+//!
+//! Define an update preserves sorting as:
+//!
+//! * `vec.set(i,v)` preserves sorting whenever, for any `j != i`,
+//!   we have `vec[i] < vec[j]` if any only if `v < vec[j]`.
+//!
+//! * `vec.push(v)` preserves sorting whenever, for any `i`,
+//!   we have `vec[i] < v`.
+//!
+//! A presorted vector is definitely sorted when its update history contains
+//! an update which establishes sorting, and every subsequent update
+//! preserves sorting.
+//!
+//! For definitely sorted vectors, `vec.presort()` and `vec.sorted_iter()` are constant time functions.
+
+/// The type of presorted vectors.
 #[derive(Clone,Debug)]
 pub struct PresortedVec<T> {
+    // The contents of the vector, possibly permuted.
     contents: Vec<T>,
+    // If this is true, contents is sorted.
     is_sorted: bool,
+    // A permutation `p`, such that `vec[i]` is the same as `vec[p[i]]`.
     permute_original_to_contents: Vec<usize>,
+    // The inverse permutation.
     permute_contents_to_original: Vec<usize>,
 }
 
 impl<T> PresortedVec<T>
 where T: Ord {
+    /// Create a new, empty presorted vector.
     pub fn new() -> PresortedVec<T> {
         PresortedVec {
             contents: Vec::new(),
@@ -16,6 +48,9 @@ where T: Ord {
             permute_contents_to_original: Vec::new(),
         }
     }
+
+    /// Make the vector definitely sorted.
+    /// If the vector is already definitely sorted, this is a constant time operation.
     pub fn presort(&mut self) {
         if !self.is_sorted {
             let permute_contents_to_original = &mut *self.permute_contents_to_original;
@@ -33,18 +68,30 @@ where T: Ord {
             }
         }
     }
+
+    /// A sorted iterator over the vector.
+    /// Makes the vector definitely sorted.
+    /// If the vector is already definitely sorted, this is a constant time operation.
     pub fn sorted_iter(&mut self) -> std::slice::Iter<T> {
         self.presort();
         self.contents.iter()
     }
+
+    /// Check to see if the vector is definitely sorted.
     pub fn is_presorted(&self) -> bool {
         self.is_sorted
     }
+
+    /// Get the `i`th element of the vector.
+    /// Returns `None` if the vector contains fewer than `i` elements.
     pub fn get(&self, original_index: usize) -> Option<&T> {
         self.permute_original_to_contents
             .get(original_index)
             .and_then(|&contents_index| self.contents.get(contents_index))
     }
+
+    /// Set the `i`th element of the vector.
+    /// Panics if the vector contains fewer than `i` elements.
     pub fn set(&mut self, original_index: usize, value: T) {
         let contents_index = self.permute_original_to_contents[original_index];
         self.is_sorted =
@@ -53,6 +100,8 @@ where T: Ord {
             self.contents.get(contents_index.wrapping_add(1)).map(|after| &value <= after).unwrap_or(true);
         self.contents[contents_index] = value;
     }
+
+    /// Append an element to the end of the vector.
     pub fn push(&mut self, value: T) {
         let original_index = self.contents.len();
         let contents_index = original_index;
@@ -63,6 +112,8 @@ where T: Ord {
         self.permute_original_to_contents.push(contents_index);
         self.permute_contents_to_original.push(original_index);
     }
+
+    /// The length of the vector.
     pub fn len(&self) -> usize {
         self.contents.len()
     }
