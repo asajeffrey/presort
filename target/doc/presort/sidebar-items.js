4e23a5af11fb93f0c62d2afe44389d25a17bb011
Alan Jeffrey <ajeffrey@mozilla.com>
2016-07-26 12:47:09 -0500

    Regenerated docs.

--- target/doc/presort/sidebar-items.js
+++ target/doc/presort/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["PresortedVec","The type of presorted vectors."]]});
+initSidebarItems({"struct":[["PresortedVec","The type of presorted vectors."],["SortedIter","The type of sorted iterators over a presorted vector."]]});
