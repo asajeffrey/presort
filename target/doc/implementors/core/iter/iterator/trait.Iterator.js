a51e4bcf260ad09be47a4d563414c79251280c5d
Alan Jeffrey <ajeffrey@mozilla.com>
2016-08-01 18:10:48 -0700

    Updated rustdoc.

---
+++ target/doc/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["presort"] = ["impl&lt;'a, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='presort/struct.SortedIter.html' title='presort::SortedIter'>SortedIter</a>&lt;'a, T&gt; <span class='where'>where T: 'a</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
