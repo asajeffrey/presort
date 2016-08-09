ed5575af76f826c6fba47bcd0b51079d9add3284
Kyle Headley <kyle.headley@colorado.edu>
2016-08-08 17:09:26 -0700

    add trait file

---
+++ src/sortvec.rs
@@ -0,0 +1,52 @@
+pub trait SortVec<T: Ord> {
+    /// The length of the vector.
+    fn len(&self) -> usize;
+
+    /// Append an element to the end of the vector.    
+    fn push(&mut self, value: T);
+    
+    /// Set the `i`th element of the vector.
+    /// Panics if the vector contains fewer than `i` elements.
+    fn set(&mut self, index: usize, value: T);
+    
+    /// Truncate this vector and reset the sort if necessary.
+    fn truncate(&mut self, len: usize);
+    
+    /// Sort the vector
+    fn sort(&mut self);
+}
+
+pub trait IntoSortedIterator {
+    type Item: Ord;
+    type IntoSortedIter: Iterator<Item=Self::Item>;
+
+    /// A sorted iterator over the vector.
+    fn sorted_iter(self) -> Self::IntoSortedIter;
+}
+
+impl<T: Ord> SortVec<T> for Vec<T> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+    fn push(&mut self, val: T) {
+        self.push(val);
+    }
+    fn set(&mut self, index: usize, val: T) {
+        self[index] = val;
+    }
+    fn truncate(&mut self, size: usize) {
+        self.truncate(size);
+    }
+    fn sort(&mut self) {
+        (**self).sort();
+    }
+}
+
+// impl<'a, T: Ord> IntoSortedIterator for &'a Vec<T> {
+//     type Item = T;
+//     type IntoSortedIter = Iter<T>;
+
+//     fn sorted_iter(self) {
+
+//     }
+// }
